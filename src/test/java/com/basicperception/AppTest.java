f2404ecc8e2d22ca05e53b680f973ba6c821c035
Nucl3arSn3k <shredderofwheat@gmail.com>
2024-10-09 15:09:33 -0400

    Updating test framework

--- src/test/java/com/basicperception/AppTest.java
+++ src/test/java/com/basicperception/AppTest.java
@@ -1,38 +1,24 @@
 package com.basicperception;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "This test should always pass");
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+   
+    @Test
+    public void testDotProduct() {
+        Perceptron perp = new Perceptron(0.1f, 10);
+        double[] X = {1.0, 2.0, 3.0};
+        double[] Y = {4.0, 5.0, 6.0};
+       
+        double expected = 32.0; // 1*4 + 2*5 + 3*6 = 4 + 10 + 18 = 32
+        double actual = perp.dot_product(X, Y);
+       
+        assertEquals(expected, actual, 0.0001, "The dot product should be 32.0");
     }
 }
